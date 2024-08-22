a0022ba9d1acead49a7954be59de3195bb3f064e
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2024-08-21 23:02:23 -0230

    tailwind data picker added

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './index.css';
-import Nav from './components/nav.jsx';
-import Home from './pages/home.jsx';
-import Login from './pages/login.jsx';
-import Signup from './pages/signup.jsx';
-import DraftSetup from './pages/draftSetup.jsx';
-import DraftEdit from './pages/draftEdit.jsx';
+import './css/index.css';
+import Nav from './components/Nav.jsx';
+import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
+import Signup from './pages/Signup.jsx';
+import DraftSetup from './pages/DraftSetup.jsx';
+import DraftEdit from './pages/DraftEdit.jsx';
 
 const App = () => (
   <Router>
--- src/pages/draftSetup.jsx
+++ src/pages/draftSetup.jsx
@@ -1,131 +1,124 @@
-import React, { useState, useEffect } from 'react';
-import { MdOutlineRemoveCircleOutline } from 'react-icons/md';
+import React, { useState } from 'react';
+import Datepicker from 'react-tailwindcss-datepicker';
 
 const DraftSetup = () => {
-  const [allPlayers, setAllPlayers] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [playerSearchResults, setPlayerSearchResults] = useState([]);
-  const [draftBoard, setDraftBoard] = useState([]);
+  const [draftName, setDraftName] = useState('');
+  const [selectedSport, setSelectedSport] = useState('');
+  const [rounds, setRounds] = useState(15);
+  const [draftDate, setDraftDate] = useState({});
+  const [teams, setTeams] = useState(12);
+  const [notes, setNotes] = useState('');
 
-  const API_KEY = import.meta.env.VITE_API_KEY;
-  const API_URL = import.meta.env.VITE_API_URL;
-
-  useEffect(() => {
-    const fetchPlayers = async () => {
-      try {
-        const cachedPlayers = localStorage.getItem('allPlayers');
-
-        if (cachedPlayers) {
-          // Use cached data if it exists
-          setAllPlayers(JSON.parse(cachedPlayers));
-          console.log('Using cached data');
-        } else {
-          console.log('Calling API...');
-          const response = await fetch(`${API_URL}?key=${API_KEY}`);
-          const data = await response.json();
-
-          // Store fetched data in localStorage
-          localStorage.setItem('allPlayers', JSON.stringify(data));
-          setAllPlayers(data);
-        }
-      } catch (error) {
-        console.error('Error fetching player data:', error);
-      }
-    };
-
-    fetchPlayers();
-  }, [API_KEY, API_URL]);
-
-  useEffect(() => {
-    if (searchTerm) {
-      setPlayerSearchResults(
-        allPlayers.filter((player) =>
-          `${player.FirstName} ${player.LastName} ${player.Team} ${player.Position}`
-            .toLowerCase()
-            .includes(searchTerm.toLowerCase())
-        )
-      );
-    } else {
-      setPlayerSearchResults([]);
-    }
-  }, [searchTerm, allPlayers]);
-
-  const handleAddPlayer = (player) => {
-    setDraftBoard([...draftBoard, player]);
-    setSearchTerm('');
+  const handleSelectChange = (event) => {
+    setSelectedSport(event.target.value);
   };
 
-  const movePlayer = (index, direction) => {
-    const newDraftBoard = [...draftBoard];
-    const temp = newDraftBoard[index];
-    newDraftBoard[index] = newDraftBoard[index + direction];
-    newDraftBoard[index + direction] = temp;
-    setDraftBoard(newDraftBoard);
+  const handleSubmit = () => {
+    console.log({
+      draftName,
+      selectedSport,
+      rounds,
+      draftDate,
+      teams,
+      notes,
+    });
   };
 
   return (
-    <div className='min-h-screen p-8 bg-base-200 text-base-content'>
-      <div className='max-w-4xl mx-auto'>
-        <h1 className='text-3xl font-bold mb-6 text-center'>Draft Room</h1>
+    <div className='flex justify-center items-center min-h-full'>
+      <div className='p-10 w-full sm:w-full md:w-3/4 lg:w-1/2'>
+        <h2 className='text-2xl font-bold mb-6 text-center'>
+          Setup Your Draft
+        </h2>
 
-        <input
-          type='text'
-          placeholder='Search for a player...'
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className='w-full p-3 mb-4 border border-neutral  rounded-lg shadow-sm text-black'
-        />
+        <div className='mb-4'>
+          <label className='block text-sm font-medium text-gray-600'>
+            Draft Name
+          </label>
+          <input
+            type='text'
+            placeholder='Enter draft name'
+            value={draftName}
+            onChange={(e) => setDraftName(e.target.value)}
+            className='input input-bordered input-primary w-full'
+          />
+        </div>
 
-        {playerSearchResults.length > 0 && (
-          <ul className=' rounded-lg shadow-lg overflow-y-auto max-h-60  border border-neutral'>
-            {playerSearchResults.map((player, index) => (
-              <li
-                key={`${player.PlayerID}-${index}`}
-                onClick={() => handleAddPlayer(player)}
-                className='p-3 border-b border-r border-neutral cursor-pointer hover:bg-accent hover:text-accent-content'
-              >
-                {player.FirstName} {player.LastName} - {player.Team} (
-                {player.Position})
-              </li>
-            ))}
-          </ul>
-        )}
+        <div className='mb-4'>
+          <label className='block text-sm font-medium text-gray-600'>
+            Select a Sport
+          </label>
+          <select
+            className='select select-primary w-full'
+            value={selectedSport}
+            onChange={handleSelectChange}
+          >
+            <option disabled value=''>
+              Select a sport
+            </option>
+            <option value='NFL'>NFL</option>
+            <option value='MLB'>MLB</option>
+            <option value='NHL'>NHL</option>
+          </select>
+        </div>
 
-        <h2 className='text-2xl font-semibold mt-8 w-full'>Your Draft Board</h2>
-        <ul className='rounded-lg shadow-lg mt-4'>
-          {draftBoard.map((player, index) => (
-            <li
-              key={`${player.PlayerID}-${index}`}
-              className='flex justify-between p-3 border border-neutral rounded-lg mb-1'
-            >
-              <div className='flex items-center'>
-                <span className='mr-2'>{index + 1}.</span>
-                <span>
-                  {player.FirstName} {player.LastName} - {player.Team} (
-                  {player.Position})
-                </span>
-              </div>
-              <div className='flex items-center'>
-                <button
-                  onClick={() => movePlayer(index, -1)}
-                  disabled={index === 0}
-                  className='mr-2'
-                >
-                  Up
-                </button>
-                <button
-                  onClick={() => movePlayer(index, 1)}
-                  disabled={index === draftBoard.length - 1}
-                >
-                  Down
-                </button>
-                <button className='ml-2 text-error'>
-                  <MdOutlineRemoveCircleOutline size={22} />
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
+        <div className='mb-4'>
+          <label className='block text-sm font-medium text-gray-600'>
+            Number of Rounds
+          </label>
+          <input
+            type='number'
+            min='1'
+            value={rounds}
+            onChange={(e) => setRounds(e.target.value)}
+            className='input input-bordered input-primary w-full'
+          />
+        </div>
+
+        <div className='mb-4'>
+          <label className='block text-sm font-medium text-gray-600'>
+            Draft Date & Time (optional)
+          </label>
+          <Datepicker
+            value={draftDate}
+            onChange={(newValue) => setDraftDate(newValue)}
+            useRange={false}
+            asSingle={true}
+            inputClassName='input input-bordered input-primary w-full'
+          />
+        </div>
+
+        <div className='mb-4'>
+          <label className='block text-sm font-medium text-gray-600'>
+            Number of Teams (optional)
+          </label>
+          <input
+            type='number'
+            min='2'
+            value={teams}
+            onChange={(e) => setTeams(e.target.value)}
+            className='input input-bordered input-primary w-full'
+          />
+        </div>
+
+        <div className='mb-6'>
+          <label className='block text-sm font-medium text-gray-600'>
+            Custom Notes (optional)
+          </label>
+          <textarea
+            value={notes}
+            onChange={(e) => setNotes(e.target.value)}
+            className='textarea textarea-bordered w-full'
+            placeholder='Any custom rules or notes?'
+          />
+        </div>
+
+        <button
+          onClick={handleSubmit}
+          className='btn btn-primary btn-block text-white shadow-sm'
+        >
+          Continue
+        </button>
       </div>
     </div>
   );
